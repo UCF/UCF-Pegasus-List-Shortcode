07e912d3aa21dc34fe4dd97d9cfeae92f69b767f
Jim Barnes <james.barnes@ucf.edu>
2017-06-22 15:49:33 -0400

    Initial commit

---
+++ ucf-pegasus-list.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: UCF Pegasus Issue List
+Description: Provides a shortcode for displaying the latest issues of Pegasus, UCF's official alumni magazine.
+Version: 1.0.0
+Author: UCF Web Communications
+License: GPL3
+*/
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
